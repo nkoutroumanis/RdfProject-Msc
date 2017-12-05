7a2788cbe039f43621c77c27a17f80a9345e9e3b
nicholaskoutroumanis <nicholaskoutroumanis@192.168.1.13>
2017-12-05 23:09:15 +0200

    String to Int is fixed

--- src/main/java/com/github/nkoutroumanis/PartitionQueryingExample.java
+++ src/main/java/com/github/nkoutroumanis/PartitionQueryingExample.java
@@ -16,29 +16,44 @@
 package com.github.nkoutroumanis;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import static org.apache.spark.api.java.StorageLevels.MEMORY_ONLY;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.hive.HiveContext;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
+import scala.reflect.api.TypeTags.TypeTag;
 
 /**
  *
  * @author nicholaskoutroumanis
  */
 public final class PartitionQueryingExample {
-    private static final String triplatesAbsolutePath = "/Users/charalambostilliros/Downloads/aisEncodedDataSample/ais_jan2016_20170329_encoded.sample.txt";//absolute path of the txt containing triplates
-    private static final int numberOfPartitions = 2;
-     public static void main(String args[]) {
+
+    private static final String triplatesAbsolutePath = "/Users/nicholaskoutroumanis/Desktop/aisEncodedDataSample/ais_jan2016_20170329_encoded.sample.txt";//absolute path of the txt containing triplates
+    private static final int numberOfPartitions = 1;
+
+    public static void main(String args[]) {
         //Initialization of Apache Spark
         SparkConf conf = new SparkConf().setMaster("local").setAppName("Spark");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        HiveContext hiveCtx = new HiveContext(sc.sc());
         //Read txt - per line and Separate every Line to a triplet of numbers
         JavaRDD<List<String>> wordsPerLine = sc.textFile(triplatesAbsolutePath).map(new Function<String, List<String>>() {
             @Override
@@ -47,31 +62,57 @@ public final class PartitionQueryingExample {
             }
 
         });
-        
+
         //Construct Pair RDD having as Key a Subject
         JavaPairRDD<Integer, List<Integer>> pairs = wordsPerLine.mapToPair(
                 new PairFunction<List<String>, Integer, List<Integer>>() {
             @Override
             public Tuple2<Integer, List<Integer>> call(List<String> x) throws Exception {
 
-                return new Tuple2(Integer.getInteger(x.get(0)), Arrays.asList(Integer.getInteger(x.get(1)),Integer.getInteger(x.get(2))));
+                return new Tuple2(Integer.parseInt(x.get(0)), Arrays.asList(Integer.parseInt(x.get(1)), Integer.parseInt(x.get(2))));
             }
         }
         );
-        
-        JavaPairRDD<Integer, List<Integer>> positiveSubjects = pairs.filter(new Function<Tuple2<Integer, List<Integer>>,Boolean>(){
-                @Override
-                public Boolean call(Tuple2<Integer, List<Integer>> tuple) {
-                    return (tuple._1>=0);
-                }            
+
+
+        JavaPairRDD<Integer, List<Integer>> positiveSubjects = pairs.filter(new Function<Tuple2<Integer, List<Integer>>, Boolean>() {
+            @Override
+            public Boolean call(Tuple2<Integer, List<Integer>> tuple) {
+                return (tuple._1 >= 0);
+            }
         }).sortByKey(true, numberOfPartitions).persist(MEMORY_ONLY);
+       System.out.println(positiveSubjects.collect());
         
-        JavaPairRDD<Integer, List<Integer>> negativeSubjects = pairs.filter(new Function<Tuple2<Integer, List<Integer>>,Boolean>(){
-                @Override
-                public Boolean call(Tuple2<Integer, List<Integer>> tuple) {
-                    return (tuple._1<0);
-                }            
-        });
+
+//        JavaPairRDD<Integer, List<Integer>> negativeSubjects = pairs.filter(new Function<Tuple2<Integer, List<Integer>>, Boolean>() {
+//            @Override
+//            public Boolean call(Tuple2<Integer, List<Integer>> tuple) {
+//                return (tuple._1 < 0);
+//            }
+//        });
+//
+//        final Broadcast<JavaPairRDD<Integer, List<Integer>>> x = sc.broadcast(negativeSubjects);
+//
+//        x.getValue().mapPartitions(new FlatMapFunction<Iterator<Tuple2<Integer,List<Integer>>>, Row>(){
+//            @Override
+//            public Iterable<Row> call(Iterator<Tuple2<Integer, List<Integer>>> t) throws Exception {
+//                t.next().
+//                        return RowFactory.create(values);
+//               
+//            }
+//            
+//        }
+//        )
+//       
+//        //contstruct the column names
+//        StructType customSchema = new StructType(new StructField[]{
+//            new StructField("Subject", DataTypes.StringType, true, Metadata.empty()),
+//            new StructField("Predice", DataTypes.StringType, true, Metadata.empty()),
+//            new StructField("Object", DataTypes.StringType, true, Metadata.empty()),});
+//JavaPairRDD.toRDD(positiveSubjects);
+//
+//        DataFrame df = hiveCtx.createDataset(JavaPairRDD.toRDD(positiveSubjects), customSchema);
         
-     }
+        
+    }
 }
